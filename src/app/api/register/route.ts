056ba85be4192c1c448be8778a4b8484ba5cfbea
YeabsraHabtu <yeabsrahabtu06@gmail.com>
2025-05-30 17:05:48 +0300

    Implement authentication and user registration API endpoints

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server"
+import { prisma } from "@/lib/db"
+import bcrypt from "bcryptjs"
+
+export async function POST(req: Request) {
+  try {
+    const { email, password, name } = await req.json()
+
+    if (!email || !password) {
+      return new NextResponse("Missing email or password", { status: 400 })
+    }
+
+    const exist = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    })
+
+    if (exist) {
+      return new NextResponse("User already exists", { status: 400 })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      }
+    })
+
+    return NextResponse.json(user)
+  } catch (error) {
+    console.error("REGISTER_ERROR", error)
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
